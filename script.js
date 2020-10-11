4b0043f08de0bc223f258eb3a8f32e64638055fc
alaa140101 <alaa140011@gmail.com>
2020-10-11 06:32:23 +0300

    Modals With JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('add-bookmark');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show Modal, Focus on Input
+function showModal() {
+  modal.classList.add('show-modal');
+  websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
